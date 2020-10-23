3513e985fbe2dc3961ae170a3d20cc58ebd42395
repl.it user <replituser@example.com>
2020-10-23 05:18:49 +0000

    5th

--- main.c
+++ main.c
@@ -1,12 +1,3 @@
 #include <stdio.h>
 int main() {
-<<<<<<< HEAD
- 
-  int a;
-  printf("life is egg\n");
-  scanf("%d", &a);
-=======
-  printf("hello world\n");
->>>>>>> parent of 1f22204... 두번째 20201023
-  return 0;
 }
